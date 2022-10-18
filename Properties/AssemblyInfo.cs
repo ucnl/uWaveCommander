d40659780d3b891427f74b9696012ceb32a908f6
Aleksandr <aleksandr.v.dikarev@gmail.com>
2022-10-18 12:31:28 +0300

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("μWaveCommander")]
+[assembly: AssemblyDescription("μWaveCommander\r\n" +
+                               "uWave Underwater acoustic modems utility application\n\n" +
+                               "Latest release: https://github.com/ucnl/uWaveCommander/releases \n" +
+                               "Support: mailto:support@unavlab.com\n\n" +
+                               "More information about uWave devices\n" +
+                               "English: https://docs.unavlab.com/underwater_acoustic_modems_en.html#uwave\n" +
+                               "Russian: https://docs.unavlab.com/underwater_acoustic_modems_ru.html#uwave\n\n" +
+                               "Project source code:\n" +
+                               "https://github.com/ucnl/uWaveCommander\n\n" +
+                               "This application is 100% open-source, and based on our projects:\n" +
+                               "https://github.com/ucnl/UCNLNav\n" +
+                               "https://github.com/ucnl/UCNLPhysics\n" +
+                               "https://github.com/ucnl/UCNLNMEA\n" +
+                               "https://github.com/ucnl/UCNLKML\n" +
+                               "https://github.com/ucnl/UCNLDrivers\n" +
+                               "https://github.com/ucnl/UCNLUI\n" +
+                               "https://github.com/ucnl/UCNLSalinity")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("μWaveCommander")]
+[assembly: AssemblyCopyright("Copyright © Underwater communication & navigation laboratory 2017-2022")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2a4dd818-4067-4672-b99c-75e7e85e2f9c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
